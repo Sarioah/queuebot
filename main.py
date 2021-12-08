7d2c41f8fbadadb12b3eb60f7c22a796d3213986
Sarioah <ayoung245@gmail.com>
2021-12-08 12:12:52 +1100

    Start of message_handler refactor
    Split it out into its own class to tidy up "main"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random, time
 from configparser import ConfigParser
 from irc_bot import irc_bot
-from queue import Queue, trunc
+from queue import Queue
+import message_handler
 
 config = ConfigParser()
 config.read(".config")
@@ -10,44 +11,13 @@ config = config["DEFAULT"]
 q = Queue(config["channel"])
 q.open()
 
-def test(*a):
-    import test
-    for i in test.data: q.addsong(*i)
-    return "Bot loaded with test queue"
+import test
+for i in test.data: q.addsong(*i)
+print("Bot loaded with test queue")
 
-commands = {"testqueue": test,
-            "sr": q.addsong,
-            "leave": q.leave,
-            "openqueue": q.open,
-            "closequeue": q.close,
-            "removesong": q.removesong,
-            "removeuser": q.removeuser,
-            "listqueue": q.listsongs,
-            "listusers": q.listusers,
-            "currentsong": q.currentsong,
-            "queue": q.status,
-            "picked": q.played,
-            "pick": q.picksong}
+m = message_handler.message_handler(config["bot_prefix"], q)
 
-def message_handler(msg):
-    chat_msg = msg.arguments[0]
-    chat_command = chat_msg.split(" ", 1)
-    chat_tags = {i["key"]: i["value"] for i in msg.tags}
-
-    global q, commands
-    print("*****\n%s: %s\n******\n" % (chat_tags["display-name"], chat_msg))
-    for k in chat_tags: print("%s: %s" % (k, chat_tags[k]))
-    try:
-        if chat_msg[:1] == config["bot_prefix"]:
-            cmd = commands[chat_command[0][1:]]
-            user = chat_tags["display-name"]
-            try: args = chat_command[1]
-            except IndexError: args = ""
-            res = cmd(user, args)
-            return trunc(res, 450)
-    except KeyError: pass
-
-bot = irc_bot(config["bot_nick"], config["tmi_token"], config["channel"], message_handler)
+bot = irc_bot(config["bot_nick"], config["tmi_token"], config["channel"], m.handle_msg)
 bot.start_bot()
 
 while not bot.joined:
@@ -58,7 +28,7 @@ print("Bot is ready for commands")
 
 while True:
     #msg = input("> ")
-    #boit.send_msg(msg)
+    #bot.send_msg(msg)
     try:
         bot.poll()
     except Exception as e:
---
+++ message_handler.py
@@ -0,0 +1,37 @@
+from queue import trunc
+
+class message_handler:
+    def __init__(self, sep, q):
+        self.sep = sep
+        self.queue = q
+        self.commands = {"testqueue": "test",
+                    "sr": "addsong",
+                    "leave": "leave",
+                    "openqueue": "open",
+                    "closequeue": "close",
+                    "removesong": "removesong",
+                    "removeuser": "removeuser",
+                    "listqueue": "listsongs",
+                    "listusers": "listusers",
+                    "currentsong": "currentsong",
+                    "queue": "status",
+                    "picked": "played",
+                    "pick": "picksong"}
+
+    def handle_msg(self, msg):
+        chat_msg = msg.arguments[0]
+        chat_command = chat_msg.split(" ", 1)
+        chat_tags = {i["key"]: i["value"] for i in msg.tags}
+
+        
+        print("*****\n%s: %s\n******\n" % (chat_tags["display-name"], chat_msg))
+        for k in chat_tags: print("%s: %s" % (k, chat_tags[k]))
+        try:
+            if chat_msg[:1] == self.sep:
+                cmd = getattr(self.queue, self.commands[chat_command[0][1:]])
+                user = chat_tags["display-name"]
+                try: args = chat_command[1]
+                except IndexError: args = ""
+                res = cmd(user, args)
+                return trunc(res, 450)
+        except KeyError: pass
