274d06c4667fc185135883d054bf58c888cd3228
Sarioah <3851003+Sarioah@users.noreply.github.com>
2021-12-10 01:04:14 +1100

    Connect to channel name specified on first argument

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random, time
+import random, time, sys
 from configparser import ConfigParser
 from irc_bot.background_bot import background_bot
 from irc_bot.irc_bot import irc_bot
@@ -13,13 +13,15 @@ config = config["DEFAULT"]
 q = Queue(config["channel"])
 q.open()
 
-import test
-for i in test.data: q.addsong(*i)
-print("Bot loaded with test queue")
+#import test
+#for i in test.data: q.addsong(*i)
+#print("Bot loaded with test queue")
 
 m = message_handler(config["bot_prefix"], q, trunc)
-bot = irc_bot(config["bot_nick"], config["tmi_token"], config["channel"], config["muted"], m.handle_msg)
+channel = sys.argv[1] if len(sys.argv) > 1 else config["channel"]
+bot = irc_bot(config["bot_nick"], config["tmi_token"], channel, config["muted"], m.handle_msg)
 bgbot = background_bot(bot)
+
 bgbot.start()
 
 def cmd(msg):
@@ -27,9 +29,8 @@ def cmd(msg):
     bot.send_msg(res)
     return res
 
-while not bot.joined:
-    bot.poll()
-print(col("Bot is ready for commands", "GREEN"))
+while not bot.joined: bot.poll()
+else: print(col("Bot is ready for commands", "GREEN"))
 
 while True:
     msg = input("> ") or '0'
