73c838e59695f2baadd596300f85489ab90b2a15
Sarioah <3851003+Sarioah@users.noreply.github.com>
2021-12-15 16:22:17 +1100

    Event / message handler cleanup

--- irc_bot/events.py
+++ irc_bot/events.py
@@ -1,21 +1,39 @@
 from tools.colours import colourise as col
 class handle_event():
-    def __init__(self, *a):
-        events = {"action"    : 0,
-                  "pubnotice" : 0,
-                  "privnotice": 0,
-                  "usernotice": 0,
-                  "whisper"   : 0}
-    def __call__(self, *a):
-        try: action = getattr(self, "on_" + a[1])
+    def __init__(self, msg):
+        self.msg = msg['msg']
+        self.words = msg['words']
+        self.tags = msg['tags']
+
+    def __call__(self, msg_type, callback):
+        try: action = getattr(self, "on_" + msg_type)
         except AttributeError: return
-        res = action(**a[0])
+        return action(callback)
+
+    def on_pubmsg(self, callback):
+        print(f"<{col(self.tags['display-name'], 'CYAN')}>{self.format_badges()}: {self.msg}")
+        return callback(self.msg, self.words, self.tags)
+    def on_action(self, *a): print(col(f"{self.tags['display-name']}: {self.msg}", "CYAN"))
+    def on_pubnotice(self, *a): print(col(self.msg, "GREY"))
+    def on_privnotice(self, *a): print(col(self.msg, "GREY"))
+    def on_usernotice(self, *a):
+        res = col(self.tags['system-msg'], "GREY" ) if 'system-msg' in self.tags else ''
+        if res: print(res + f" - {self.msg or '<no msg>'}")
+    def on_whisper(self, *a): print(col(f"Whisper from {self.tags['display-name']}: {self.msg}", "GREY"))
+
+    def format_badges(self):                                                                       
+        badges = self.tags['badges']                                                                  
+        res = ''                                                                                        
+        roles = [("RED", ("broadcaster",), "B"),                                                        
+                 ("GREEN", ("moderator",), "M"),                                                        
+                 ("BLUE", ("subscriber", "premium"), "S"),                                              
+                 ("PURPLE", ("vip",), "V")]                                                             
+        for role in roles:                                                                              
+            res += col(role[2], role[0]) if role_check(badges, *role[1]) else ''                   
         return res
 
-    def on_action(self, msg, words, tags): return col(f"{tags['display-name']}: {msg}", "CYAN")
-    def on_pubnotice(self, msg, words, tags): return col(msg, "GREY")
-    def on_privnotice(self, msg, words, tags): return col(msg, "GREY")
-    def on_usernotice(self, msg, words, tags):
-        res = col(tags['system-msg'], "GREY" ) if 'system-msg' in tags else ''
-        if res: return res + f" - {msg or '<no msg>'}"
-    def on_whisper(self, msg, words, tags): return col(f"Whisper from {tags['display-name']}: {msg}", "GREY")
+def role_check(badges, *roles):                                     
+    roles = roles or ("moderator", "broadcaster")                         
+    try: return any([role in badge for badge in badges.split(",")         
+                        for role in roles])                               
+    except AttributeError: return False
