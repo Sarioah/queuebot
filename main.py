89d4fca23b65e09ae8e450a40c39d6d0c4610ac8
Aiden Young <ayoung245@gmail.com>
2021-12-17 12:04:07 +1000

    Added .exe icon, output colouring in CMD, first attempt at close handler

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random, time, sys, os
+import random, time, sys, os, colorama
 from configparser import ConfigParser
 from irc_bot.background_bot import background_bot
 from irc_bot.irc_bot import irc_bot
@@ -6,7 +6,27 @@ from irc_bot.message_handler import message_handler
 from tools.colours import colourise as col
 from tools.Queue import Queue, trunc
 
+def cmd(msg):
+    res = m.run_cmd(msg)
+    bot.send_msg(res)
+    return res
+
+def catch_exit(*a): raise KeyboardInterrupt
+
+def close(*a):
+    bgbot.quit()
+    m.shelve.close()
+    print("Exiting...")
+    time.sleep(3)
+    return False
+
 if not os.path.isdir("data"): os.mkdir("data")
+
+colorama.init()
+
+if os.name == "nt":
+    import win32api
+    win32api.SetConsoleCtrlHandler(close, True)
 
 config = ConfigParser()
 config.read(".config")
@@ -21,11 +41,6 @@ m = message_handler(channel, config["bot_prefix"], trunc)
 bot = irc_bot(config["bot_nick"], config["tmi_token"], channel, config["muted"], m.handle_msg)
 bgbot = background_bot(bot)
 
-def cmd(msg):
-    res = m.run_cmd(msg)
-    bot.send_msg(res)
-    return res
-
 while not bot.joined: bot.poll()
 else: print(col("Bot is ready for commands", "GREEN"))
 
@@ -35,13 +50,9 @@ while True:
         res = eval(msg)
         print(res)
     except (KeyboardInterrupt, EOFError):
-        bgbot.quit()
-        break
+        sys.exit()
     except Exception as e:
         import traceback
         print("oh no....anyway")
         traceback.print_exc()
     time.sleep(1)
-
-m.shelve.close()
-print("Exiting...")
