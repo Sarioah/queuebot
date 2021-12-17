5bb68b42401bcb8d023b6451a949f2418432366f
Sarioah <3851003+Sarioah@users.noreply.github.com>
2021-12-17 12:04:07 +1000

    Added .exe icon, output colouring in CMD, first attempt at close handler

--- package.bat
+++ package.bat
@@ -3,4 +3,5 @@ py -m nuitka --standalone ^
 	     --remove-output ^
 	     --include-package-data=jaraco.text ^
 	     -o "sari queuebot.exe" ^
+	     --windows-icon-from-ico=.\robot.ico ^
 	     .\main.py
