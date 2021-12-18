596ab5f7c26827e002a527cce22f6c76787025c7
sarioah <3851003+Sarioah@users.noreply.github.com>
2021-12-19 00:53:08 +1000

    Bot writes default config entries with more granularity

--- tools/config.py
+++ tools/config.py
@@ -16,6 +16,11 @@ class configuration():
         except ParsingError:
             self._config_bad()
 
+        for k in defaults:
+            if k not in self.c['DEFAULT']: self.c['DEFAULT'][k] = defaults[k]
+        with open(self.configfile, "w") as fd:
+            self.c.write(fd)
+
         if any([True for k in self.c['DEFAULT'] if self.c['DEFAULT'][k] == "********"]):
             self._config_empty("Default fields need to be filled out in")
         if not self.c['DEFAULT']:
@@ -24,25 +29,19 @@ class configuration():
     def get_config(self): return self.c['DEFAULT']
 
     def _config_empty(self, msg):
-        for k in defaults:
-            if k not in self.c['DEFAULT']: self.c['DEFAULT'][k] = defaults[k]
-
-        with open(self.configfile, "w") as fd:
-            self.c.write(fd)
-
         res = [f"{msg} '{col(self.configfile, 'YELLOW')}'","",
                f"Please log into twitch using your bot account, then visit {col('https://twitchapps.com/tmi', 'BLUE')} to " +
                f"generate an oauth code for the bot to login with. This code needs to be entered into the config file " +
                f"('{col(self.configfile, 'YELLOW')}') along with a couple other values:","",
-               f"     {col('TMI_TOKEN', 'GREEN')}  : Oauth code, including the 'oauth:' part at the front",
-               f"     {col('BOT_NICK', 'GREEN')}   : Name of the twitch account the bot will login with",
-               f"     {col('CHANNEL', 'GREEN')}    : Name of the twitch channel the bot will listen in, and send messages to",
-               f"     {col('BOT_PREFIX', 'GREEN')} : Symbol that should appear at the front of bot commands in chat. Default is '!'",
-               f"     {col('MUTED', 'GREEN')}      : Mutes the bot if you need to stop it sending messages","",
-               f"Once these are filled in, start the bot again."]
+               f"     {col('tmi_token', 'GREEN')}  : Oauth code, including the 'oauth:' part at the front",
+               f"     {col('bot_nick', 'GREEN')}   : Name of the twitch account the bot will login with",
+               f"     {col('channel', 'GREEN')}    : Name of the twitch channel the bot will listen in, and send messages to",
+               f"     {col('bot_prefix', 'GREEN')} : Symbol that should appear at the front of bot commands in chat. Default is '!'",
+               f"     {col('muted', 'GREEN')}      : Mutes the bot if you need to stop it sending messages","",
+               f"Once these are filled in, restart the bot."]
         raise Exception("\n".join(res))
 
     def _config_bad(self):
         res = [f"Config file '{col(self.configfile, 'YELLOW')}' appears to be misformatted.",
-               f"Please correct the error or delete the file"]
+               f"Please correct the error or delete the file, then restart the bot."]
         raise Exception("\n".join(res))
