35b116b87ed6a6dd0c26182414efb3380913cb42
Sarioah <ayoung245@gmail.com>
2021-12-19 16:36:11 +1100

    Updated linux packaging

--- package.bat
+++ package.bat
@@ -2,7 +2,7 @@ py -m nuitka --standalone ^
 	     --onefile ^
 	     --remove-output ^
 	     --include-package-data=jaraco.text ^
-	     -o "sari queuebot.exe" ^
+	     -o sari_queuebot.exe ^
 	     --windows-icon-from-ico=.\robot.ico ^
 	     --windows-onefile-tempdir-spec=".\Resources" ^
 	     .\main.py
--- package.sh
+++ package.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python3 -m nuitka --standalone --onefile --remove-output --include-package-data=jaraco.text ./main.py
+python3 -m nuitka --standalone \
+		  --onefile \
+		  --remove-output \
+		  --include-package-data=jaraco.text \
+		  -o sari_queuebot \
+		  --linux-onefile-icon=./robot.png \
+		  ./main.py
