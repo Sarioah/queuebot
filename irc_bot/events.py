b35e5e67feed3b49dac722523b4491e9ce78ee59
Sarioah <3851003+Sarioah@users.noreply.github.com>
2021-12-19 20:03:30 +1100

    Moved badge / role handling to module

--- irc_bot/events.py
+++ irc_bot/events.py
@@ -1,4 +1,6 @@
+from tools.chat import format_badges
 from tools.colours import colourise as col
+
 class handle_event():
     def __init__(self, msg):
         self.msg = msg['msg']
@@ -11,7 +13,7 @@ class handle_event():
         return action(callback)
 
     def on_pubmsg(self, callback):
-        print(f"<{col(self.tags['display-name'], 'CYAN')}>{self.format_badges()}: {self.msg}")
+        print(f"<{col(self.tags['display-name'], 'CYAN')}>{format_badges(self.tags)}: {self.msg}")
         return callback(self.msg, self.words, self.tags)
     def on_action(self, *a): print(col(f"{self.tags['display-name']}: {self.msg}", "CYAN"))
     def on_pubnotice(self, *a): print(col(self.msg, "GREY"))
@@ -21,21 +23,3 @@ class handle_event():
         if res: print(res + f" - {self.msg or '<no msg>'}")
     def on_whisper(self, *a):
         print(col(f"Whisper from {self.tags['display-name']}: {self.msg}", "GREY"))
-
-    def format_badges(self):
-        badges = self.tags['badges']
-        res = ''
-        roles = [("RED", ("broadcaster",), "B"), 
-                 ("GREEN", ("moderator",), "M"),                                        
-                 ("BLUE", ("subscriber", "premium"), "S"),                              
-                 ("PURPLE", ("vip",), "V")]
-
-        for role in roles:
-            res += col(role[2], role[0]) if role_check(badges, *role[1]) else ''
-        return res
-
-def role_check(badges, *roles):                                     
-    roles = roles or ("moderator", "broadcaster")                         
-    try: return any([role in badge for badge in badges.split(",")         
-                        for role in roles])                               
-    except AttributeError: return False
