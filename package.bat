574080bdf321c59e08dd957947e3c940fda68a84
Sarioah <3851003+Sarioah@users.noreply.github.com>
2021-12-22 15:19:26 +1000

    Fix bot closing too fast on error, update versioning

--- package.bat
+++ package.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-git describe --tags >VERSION
+for /f "usebackq tokens=*" %%a in (`git describe --tags`) do echo version = '%%a' >tools\version.py
 
 py .\tools\check_imports.py
 if %ERRORLEVEL% GTR 0 exit
@@ -8,6 +8,7 @@ py -m nuitka --standalone ^
 	     --onefile ^
 	     --remove-output ^
 	     --mingw64 ^
+	     --include-module=win32timezone ^
 	     --include-package-data=jaraco.text ^
 	     -o sari_queuebot.exe ^
 	     --windows-icon-from-ico=.\robot.ico ^
