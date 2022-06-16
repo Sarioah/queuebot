e4bc93f7e80b79f4076f9d7961145ed855cf174c
Sarioah <3851003+Sarioah@users.noreply.github.com>
2022-06-17 01:25:20 +1000

    Initial pass on tools.chat

--- tools/chat.py
+++ tools/chat.py
@@ -3,80 +3,88 @@ import time
 from tools.text import colourise as col
 
 
-class CommandFound(Exception):
-    """Signals that a command was found"""
-    pass
-
-
-class Commands():
+class Commands:
     """Object holding generic commands that aren't bound methods elsewhere"""
-
     def __init__(self, parent):
         """Creates a Commands object"""
         self.parent = parent
 
-    def help(self, *a):
+    def help(self, *_args):
         """Lists available chat commands"""
         everyone = [
-                k for k in self.parent.commands
-                if self.parent.commands[k][0] == "e"
-                ]
+            k for k in self.parent.commands
+            if self.parent.commands[k][0] == "e"
+        ]
         mods = [
-                k for k in self.parent.commands
-                if self.parent.commands[k][0] == "m"
-                ]
+            k for k in self.parent.commands
+            if self.parent.commands[k][0] == "m"
+        ]
         return (
-                f"Queuebot commands: \"{', '.join(everyone)}\" "
-                + f"• Queuebot moderator commands: \"{', '.join(mods)}\""
+            f"Queuebot commands: \"{', '.join(everyone)}\" "
+            + f"• Queuebot moderator commands: \"{', '.join(mods)}\""
+        )
+
+    def listaliases(self, *_args):
+        """Lists available command aliases"""
+        # TODO: Need to guard this and help against IRC msg limit
+
+        return (
+            "Aliases: "
+            + " • ".join(
+                ", ".join(
+                    alias for alias in value
                 )
+                + f" -> \"{key}\""
+                for key, value in self.parent.aliases.items()
+            )
+        )
 
 
-class CommandHandler():
-
+class CommandHandler:
     def __init__(self):
         """Creates a CommandHandler object"""
         self.mthds = Commands(self)
         self.commands = {
-                "sr"           : ("e", "addentry",      0),
-                "leave"        : ("e", "leave",         0),
-                "openqueue"    : ("m", "open",          3),
-                "closequeue"   : ("m", "close",         3),
-                "clearqueue"   : ("m", "clear",         3),
-                "removesong"   : ("m", "removeentry",   3),
-                "removeuser"   : ("m", "removeuser",    3),
-                "listqueue"    : ("e", "listentries",   3),
-                "listusers"    : ("m", "listusers",     5),
-                "jbqueue"      : ("m", "jbqueue",       3),
-                "jdqueue"      : ("m", "jdqueue",       3),
-                "currentsong"  : ("e", "currententry", 10),
-                "queue"        : ("e", "status",        0),
-                "played"       : ("e", "picked",        3),
-                "pick"         : ("m", "pickentry",     3),
-                "help"         : ("e", "help",         10),
-                "testqueue"    : ("m", "testqueue",     5),
-                "queueconfirm" : ("m", "queueconfirm",  0),
-                }
+            "sr"           : ("e", "addentry",      0),
+            "leave"        : ("e", "leave",         0),
+            "openqueue"    : ("m", "open",          3),
+            "closequeue"   : ("m", "close",         3),
+            "clearqueue"   : ("m", "clear",         3),
+            "removesong"   : ("m", "removeentry",   3),
+            "removeuser"   : ("m", "removeuser",    3),
+            "listqueue"    : ("e", "listentries",   3),
+            "listusers"    : ("m", "listusers",     5),
+            "jbqueue"      : ("m", "jbqueue",       3),
+            "jdqueue"      : ("m", "jdqueue",       3),
+            "currentsong"  : ("e", "currententry", 10),
+            "queue"        : ("e", "status",        0),
+            "played"       : ("e", "picked",        3),
+            "pick"         : ("m", "pickentry",     3),
+            "help"         : ("e", "help",         10),
+            "listaliases"  : ("e", "listaliases",  10),
+            "testqueue"    : ("m", "testqueue",     5),
+            "queueconfirm" : ("m", "queueconfirm",  0),
+        }
         self.cooldowns = {}
         self.aliases = {
-                "sr"          : ("join",),
-                "listqueue"   : ("queuelist",),
-                "played"      : ("picked",),
-                "removesong"  : ("removeentry",),
-                "currentsong" : ("currentuser",),
-                "testqueue"   : ("queuetest",),
-                "queueconfirm": ("confirmqueue",),
-                }
+            "sr"          : ("join",),
+            "listqueue"   : ("queuelist",),
+            "played"      : ("picked",),
+            "removesong"  : ("removeentry",),
+            "currentsong" : ("currentuser",),
+            "testqueue"   : ("queuetest",),
+            "queueconfirm": ("confirmqueue",),
+        }
 
     def __getitem__(self, attr):
         """Searches for a method maching the given request"""
         return getattr(self.mthds, attr, None)
 
     def check_aliases(self, command):
-
         res = [
-                k for k in self.aliases
-                if command in self.aliases[k]
-                ]
+            k for k, v in self.aliases.items()
+            if command in v
+        ]
         return (res[0] if res else command)
 
     def check_cooldowns(self, command):
@@ -85,15 +93,14 @@ class CommandHandler():
         if timeleft >= 0:
             self.cooldowns[command[1]] = current
             return True
-        else:
-            print(
-                    col(
-                        f"'{command[1]}' still on cooldown for "
-                        + f"{round(timeleft, 1) * -1}s",
-                        "GREY"
-                        )
-                    )
-            return False
+        print(
+            col(
+                f"'{command[1]}' still on cooldown for "
+                + f"{round(timeleft, 1) * -1}s",
+                "GREY"
+            )
+        )
+        return False
 
     def find_command(self, badges, request, *alternatives):
         """
@@ -104,37 +111,29 @@ class CommandHandler():
         """
         request = self.check_aliases(request.lower())
         command = self.commands.get(request, None)
-        if command:
-            if self.check_cooldowns(command):
-                try:
-                    for obj in (self,) + alternatives:
-                        mthd = obj[command[1]]
-                        if mthd:
-                            raise CommandFound()
-                except CommandFound:
+        if command and self.check_cooldowns(command):
+            for obj in (self,) + alternatives:
+                mthd = obj[command[1]]
+                if mthd:
                     if command[0] == "m":
                         if role_check(badges):
                             return mthd
-                        else:
-                            return None
-                    else:
-                        return mthd
-            else:
-                return None
-        else:
-            return None
+                    return mthd
+        return None
 
 
 def format_badges(tags):
     """Checks a tags dict for roles and outputs coloured markers if found"""
+    # TODO: roles as dict maybe, probably a comprehension to generate the
+    # coloured suffix
     badges = tags['badges']
     res = ''
     roles = [
-            ("RED",    ("broadcaster",),          "B"),
-            ("GREEN",  ("moderator",),            "M"),
-            ("BLUE",   ("subscriber", "premium"), "S"),
-            ("PURPLE", ("vip",),                  "V"),
-            ]
+        ("RED",    ("broadcaster",),          "B"),
+        ("GREEN",  ("moderator",),            "M"),
+        ("BLUE",   ("subscriber", "premium"), "S"),
+        ("PURPLE", ("vip",),                  "V"),
+    ]
 
     for role in roles:
         res += (col(role[2], role[0]) if role_check(badges, *role[1]) else '')
@@ -146,10 +145,10 @@ def role_check(badges, *roles):
     if not roles:
         roles = ["moderator", "broadcaster"]
     try:
-        return any([
-                role in badge
-                for badge in badges.split(",")
-                for role in roles
-                ])
+        return any(
+            role in badge
+            for badge in badges.split(",")
+            for role in roles
+        )
     except AttributeError:
         return False
