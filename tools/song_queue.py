4b9f7f2dc52601ce1ed8405b385c9c9c224bed64
Sarioah <3851003+Sarioah@users.noreply.github.com>
2022-07-10 01:24:37 +1000

    Add documentation to tools.song_queue

--- tools/song_queue.py
+++ tools/song_queue.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+Tools for managing a random or prioritised request queue
+
+In Just Dance mode, the queue holds song choices, and picks users at random
+(prioritising users who haven't been picked before)
+In Jackbox mode, the queue holds usernames, and picks users FIFO (also
+prioritising users who haven't been picked before)
+
+Classes:
+    BaseMethods: Class containing methods to manipulate all types of queues
+    JDMethods: Class containing methods to manipulate Just Dance / random queues
+    JBMethods: Class containing methods to manipulate Jackbox / priority queues
+    SongQueue: Class containing users / requests
+
+Functions:
+    trunc: Take a string then truncate it to the specified length
+"""
+
 import time
 
 from os import path
@@ -11,10 +29,11 @@ from tools.text import Paginate
 
 
 SINGLE_SONG_LENGTH = 200
-MULTI_SONG_LENGTH = 50 # (for lists)
+MULTI_SONG_LENGTH = 50  # (for lists)
 
 
 class BaseMethods:
+    """Contain methods common to all types of queues"""
     def __init__(self, parent):
         self.parent = parent
 
@@ -22,21 +41,25 @@ class BaseMethods:
         return getattr(self, key, None)
 
     def close(self, *_args):
+        """Close the queue, disallowing further entries"""
         self.parent.isopen = False
         return "Queue is now closed"
 
     def clear(self, *_args):
+        """Remove all entries from the queue"""
         self.parent.current = {}
         self.parent.entries, self.parent.picked = TupleList(), TupleList()
         return "Queue has been cleared"
 
     def leave(self, sender, /, *_args):
+        """Remove the sender's request from the queue"""
         if sender in self.parent:
             del self.parent.entries[sender]
             return f"{sender}, you have left the queue"
         return f"{sender}, you weren't in the queue"
 
     def listusers(self, _, page=1, /, *_args):
+        """List all users who have a request in the queue"""
         if not self.parent.entries:
             return "Queue is empty"
         msg = "List of users in the queue: "
@@ -45,6 +68,7 @@ class BaseMethods:
         return Paginate(msg[:-2], self.parent.msg_limit, ", ")[page]
 
     def removeuser(self, _, user="", /, *_args):
+        """Remove the specified user from the queue"""
         try:
             if user:
                 del self.parent.entries[user]
@@ -58,6 +82,7 @@ class BaseMethods:
             return f"Removed {user} from the queue"
 
     def queueconfirm(self, *_args):
+        """Confirm a prior "testqueue" command, then add the loaded data into the queue"""
         try:
             if time.time() - self.parent.testdata[1] < 10:
                 self.parent.entries = TupleList(*self.parent.testdata[0])
@@ -69,6 +94,21 @@ class BaseMethods:
         return None
 
     def testqueue(self, *_args):
+        """
+        Load testing data from "testdata.json", ready to be added with "queueconfirm"
+
+        Example file contents:
+        [
+            [
+                "Test user 1",
+                "Test user 1's request"
+            ],
+            [
+                "Test user 2",
+                "Test user 2's request"
+            ]
+        ]
+        """
         if path.exists('testdata.json'):
             try:
                 with open("testdata.json", "r", encoding="utf-8") as file_:
@@ -88,18 +128,23 @@ class BaseMethods:
 
 
 class JDMethods(BaseMethods):
+    """Contain methods used to manipulate Just Dance / random queues"""
     def jbqueue(self, *_args):
+        """Change queue into Jackbox / priority mode"""
         self.parent.mthds = JBMethods(self.parent)
         return "Queue is now in jackbox mode"
 
     def jdqueue(self, *_args):
+        """Change queue into Just Dance / random mode"""
         return "Queue is already in Just Dance mode"
 
     def open(self, *_args):
+        """Open the queue, allowing new entries to be added"""
         self.parent.isopen = True
         return "Random queue is now open"
 
     def currententry(self, *_args):
+        """List the last entry that was picked"""
         if self.parent.current:
             return (
                 f"Current song is \"{trunc(self.parent.current['entry'], SINGLE_SONG_LENGTH)}"
@@ -108,6 +153,11 @@ class JDMethods(BaseMethods):
         return "Nothing's been played yet"
 
     def addentry(self, sender, entry, emote_indices, /, *_args):
+        """
+        Add the sender's entry to the queue
+
+        Blank entries not allowed in this mode
+        """
         if not self.parent:
             return f"Sorry {sender}, the queue is closed"
         if not entry:
@@ -133,6 +183,7 @@ class JDMethods(BaseMethods):
         return f"Added \"{trunc(entry, SINGLE_SONG_LENGTH)}\" to the queue for {sender}"
 
     def removeentry(self, _, index, /, *_args):
+        """Remove the sender's entry from the queue"""
         try:
             _user, entry = self.parent.entries.pop(int(index) - 1)
         except ValueError:
@@ -143,6 +194,12 @@ class JDMethods(BaseMethods):
             return f"Removed \"{trunc(entry, SINGLE_SONG_LENGTH)}\" from the queue"
 
     def status(self, sender, /, *_args):
+        """
+        List the status of the queue
+
+        Shows whether the queue is open / closed, how many requests there are,
+        as well as the sender's current request (if applicable)
+        """
         msg = f"Random queue is {'open' if self.parent else 'closed'}"
 
         if self.parent.entries:
@@ -158,6 +215,7 @@ class JDMethods(BaseMethods):
         return msg
 
     def listentries(self, _, page=1, /, *_args):
+        """List all entries currently in the queue"""
         if not self.parent.entries:
             return "Queue is empty"
         msg = "List of songs in the queue: "
@@ -166,6 +224,7 @@ class JDMethods(BaseMethods):
         return Paginate(msg[:-3], self.parent.msg_limit, " • ")[page]
 
     def picked(self, _, page=1, /, *_args):
+        """List all entries that have been picked so far"""
         if not self.parent.picked:
             return "Nothing's been played yet"
         msg = "Songs already played: "
@@ -174,6 +233,14 @@ class JDMethods(BaseMethods):
         return Paginate(msg[:-2], self.parent.msg_limit, ", ")[page]
 
     def pickentry(self, _, selection=0, /, *_args):
+        """
+        Pick an entry from the queue
+
+        If a number is specified, pick that entry directly
+
+        If no entry is specified, pick an entry at random. Entries from users not in the
+        picked list are chosen first
+        """
         try:
             if selection:
                 user, song = self.parent.entries.pop(int(selection) - 1)
@@ -200,18 +267,23 @@ class JDMethods(BaseMethods):
 
 
 class JBMethods(BaseMethods):
+    """Contains methods used to manipulate Jackbox / priority queues"""
     def jbqueue(self, *_args):
+        """Change queue to Jackbox / priority mode"""
         return "Queue is already in Jackbox mode"
 
     def jdqueue(self, *_args):
+        """Change queue to Just Dance / random mode"""
         self.parent.mthds = JDMethods(self.parent)
         return "Queue changed to Just Dance mode"
 
     def open(self, *_args):
+        """Open the queue, allowing new entries to be added"""
         self.parent.isopen = True
         return "Priority queue is now open"
 
     def currententry(self, *_args):
+        """List the last entry to be picked"""
         if self.parent.current:
             return (
                 f"Current player is {trunc(self.parent.current['user'], SINGLE_SONG_LENGTH)}"
@@ -219,6 +291,13 @@ class JBMethods(BaseMethods):
         return "No-one's been picked yet"
 
     def addentry(self, sender, /, *_args):
+        """
+        Add the sender to the queue
+
+        if the sender has already been picked, insert their entry after any new users
+
+        The request itself is set to the sender's name
+        """
         if not self.parent:
             return f"Sorry {sender}, the queue is closed"
         if sender in self.parent.entries:
@@ -238,6 +317,7 @@ class JBMethods(BaseMethods):
         return msg
 
     def removeentry(self, _, index, /, *_args):
+        """Remove the sender from the queue"""
         try:
             user, _entry = self.parent.entries.pop(int(index) - 1)
         except ValueError:
@@ -248,6 +328,11 @@ class JBMethods(BaseMethods):
             return f"Removed {user} from the queue"
 
     def status(self, sender="", /, *_args):
+        """
+        List the status of the queue
+        Shows whether the queue is open / closed, how many users there are,
+        as well as the sender's current position in the queue
+        """
         msg = f"Priority queue is {'open' if self.parent else 'closed'}"
 
         if self.parent.entries:
@@ -264,6 +349,7 @@ class JBMethods(BaseMethods):
         return msg
 
     def listentries(self, _, page=1, /, *_args):
+        """List all users currently in the queue"""
         if not self.parent.entries:
             return "Queue is empty"
         msg = "List of users in the queue: "
@@ -272,6 +358,7 @@ class JBMethods(BaseMethods):
         return Paginate(msg[:-3], self.parent.msg_limit, " • ")[page]
 
     def picked(self, _, page=1, /, *_args):
+        """List all users that have been picked so far"""
         if not self.parent.picked:
             return "No-one's been picked yet"
         msg = "Users already picked: "
@@ -280,6 +367,13 @@ class JBMethods(BaseMethods):
         return Paginate(msg[:-2], self.parent.msg_limit, ", ")[page]
 
     def pickentry(self, _, selection=0, /, *_args):
+        """
+        Pick a user from the queue
+
+        If a number is specified, pick that user specifically
+
+        If no user is specified, pick the first user in the queue
+        """
         try:
             if selection:
                 user, entry = self.parent.entries.pop(int(selection) - 1)
@@ -306,6 +400,7 @@ class JBMethods(BaseMethods):
 
 
 class SongQueue:
+    """Main class to hold all data relating to a request queue"""
     def __init__(self, channel, *tuples):
         self.channel = None
         self.isopen = None
@@ -332,12 +427,14 @@ class SongQueue:
         return len(self.entries)
 
     def new(self, channel, *tuples):
+        """Create the queue from scratch"""
         self.channel = channel
         self.isopen = True
         self.current, self.entries, self.picked = {}, TupleList(*tuples), TupleList()
         self.save()
 
     def save(self):
+        """Dump all queue data to a file in the data/ folder"""
         with open(f"data/{self.channel}.json", "w", encoding="utf-8") as file_:
             res = {}
             res["channel"] = self.channel
@@ -348,6 +445,11 @@ class SongQueue:
             file_.write(dumps(res, indent=4))
 
     def load(self, channel, *tuples):
+        """
+        Load existing queue data from file
+
+        Create queue from scratch if data can't be loaded
+        """
         try:
             with open(f"data/{channel}.json", "r", encoding="utf-8") as file_:
                 res = loads(file_.read())
@@ -366,4 +468,14 @@ class SongQueue:
 
 
 def trunc(msg, length):
+    """
+    Take a string and truncate it to the specfied length
+
+    Arguments:
+        msg: the input string
+        length: the length to truncate the input string to
+
+    Returns:
+        the truncated string, ending in the ellipses character if truncation occurred
+    """
     return msg if len(msg) <= length else msg[:length - 1] + "…"
