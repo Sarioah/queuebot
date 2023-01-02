c3b71e2611ccc82bef6eadd09d6e88445fc35192
Sarioah <3851003+Sarioah@users.noreply.github.com>
2023-01-03 01:03:53 +1100

    Implement tests for TupleList class

--- tests/tools/__init__.py
+++ tests/tools/__init__.py
@@ -3,4 +3,6 @@
 Modules:
     test_timed_list: Test the timed list, verifying that it correctly wipes its
         data.
+    test_tuple_list: Test the TupleList, verifying that it holds and presents
+        the expected data.
 """
