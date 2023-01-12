bf8588e22faa4c22e66a4430a339263a6b5b4911
Sarioah <3851003+Sarioah@users.noreply.github.com>
2023-01-12 17:05:44 +1100

    Tweaks to testing script / tests
    - Reduce delay when testing TimedLists
    - End test script with a pass/fail to avoid scrolling

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
 python3 -m coverage run --source=. -m unittest -v $@
+RESULT="$?"
 python3 -m coverage report -m
+if [ ${RESULT} = 0 ]; then
+	echo "[32;1mTests completed sucessfully[0m"
+else
+	echo "[31;1mTests failed with code [0m${RESULT}[31;1m.[0m"
+fi
