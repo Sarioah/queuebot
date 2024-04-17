c0b2d9a7ec09c9e89952198f7b69f637304e5299
Sarioah <3851003+Sarioah@users.noreply.github.com>
2024-04-17 19:18:02 +1000

    Update version update logic to support poetry

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -4,18 +4,30 @@ import subprocess
 
 
 def update_version():
-    """Generate file containting bot version number as per git.
+    """Generate file containing bot version number as per git.
 
     Check current bot version using git, then generate a file
     that the main function can load later.
+
+    Version number is normalised and then passed to poetry to
+    update the pyproject.toml.
     """
     version = (
         subprocess.check_output("git describe --tags".split())
         .decode(encoding="utf-8")
         .replace("\n", "")
     )
+    segments = version.split("-")
+    if len(segments) > 1:
+        chunks = segments[0].split(".")
+        segments[0] = f"{chunks[0]}.{chunks[1]}.{int(chunks[2])+1}"
+        version = f"{segments[0]}.dev{segments[1]}+{segments[2]}"
+    else:
+        version = segments[0]
+
     with open("./version.txt", "w", encoding="utf-8") as _fd:
         _fd.write(version)
+    subprocess.call(f"poetry version {version}".split())
 
 
 if __name__ == "__main__":
