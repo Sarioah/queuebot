19c6dd40827edf904d322b8f4e984515e02364c6
Sarioah <3851003+Sarioah@users.noreply.github.com>
2026-01-22 15:58:33 +1000

    Swap from poetry to uv Initial pyoxidizer config added

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -21,14 +21,14 @@ def update_version(path="."):
     segments = version.split("-")
     if len(segments) > 1:
         chunks = segments[0].split(".")
-        segments[0] = f"{chunks[0]}.{chunks[1]}.{int(chunks[2])+1}"
+        segments[0] = f"{chunks[0]}.{chunks[1]}.{int(chunks[2]) + 1}"
         version = f"{segments[0]}.dev{segments[1]}+{segments[2]}"
     else:
         version = segments[0]
 
     with open(os.path.join(path, "version.txt"), "w", encoding="utf-8") as _fd:
         _fd.write(version)
-    subprocess.call(f"poetry version {version}".split())
+    subprocess.call(f"uv version {version}".split())
 
 
 if __name__ == "__main__":
