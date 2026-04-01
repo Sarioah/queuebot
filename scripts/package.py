3c7e351d4178d1b35cc41be5cba4209cbaba5810
Sarioah <3851003+Sarioah@users.noreply.github.com>
2026-04-01 21:43:05 +1000

    Use sys.executable instead to better suit venv / uv contexts

--- scripts/package.py
+++ scripts/package.py
@@ -1,14 +1,12 @@
 """Handle packaging the queuebot into a standalone executable file."""
 
 import os
+import sys
 from subprocess import call
-
-from scripts.script_tools import valid_command
 
 from .update_version import update_version
 
 PLATFORM = "win" if os.name == "nt" else "linux"
-PYTHON = "py" if valid_command("py --version") else "python3"
 PARAMETERS = {
     "base": [
         "--standalone",
@@ -24,7 +22,6 @@ PARAMETERS = {
     ],
     "win": [
         "--mingw64",
-        "--include-module=win32timezone",
         r"--windows-icon-from-ico=.\robot.ico",
         "-o",
         "sari_queuebot.exe",
@@ -43,7 +40,7 @@ def build():
     """Run nuitka with platform specific arguments to build the bot binary."""
     print("Running build tool...")
     update_version()
-    call([PYTHON, "-m", "nuitka"] + PARAMETERS["base"] + PARAMETERS[PLATFORM])
+    call([sys.executable, "-m", "nuitka"] + PARAMETERS["base"] + PARAMETERS[PLATFORM])
 
 
 if __name__ == "__main__":
